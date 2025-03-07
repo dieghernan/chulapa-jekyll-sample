a70906bc0b40e9d01a320a4984821339e0cc9ac9
dieghernan <diego.hernangomezherrero@gmail.com>
2025-03-07 14:08:31 +0000

    Deploying to gh-pages from @ dieghernan/chulapa-jekyll-sample@c299c5c934ed56ba2928769ad62e70dcb03906f2 🚀

--- assets/js/fusejs/fusejs-store.js
+++ assets/js/fusejs/fusejs-store.js
@@ -613,4 +613,154 @@ var store = [{
         "tags": [],
         "url": "https://dieghernan.github.io/chulapa-jekyll-sample/pets/tabby",
         "img": null
+      },{
+        "title": "Chocolate Chip Cookies",
+        "subtitle": "",
+        "excerpt": "A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient.",
+        "content": "A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient.   The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as well as additional ingredients such as nuts or oatmeal.   This recipe makes 4 dozen cookies.   Ingredients      2 1/4 cups all-purpose flour   1 teaspoon baking soda   1/2 teaspoon salt   1 cup butter, softened and cut to pieces   1 cup sugar   1 cup light brown sugar, packed   2 teaspoons vanilla extract   2 large eggs   2 cups semi-sweet chocolate chips   1/2 teaspoon nutmeg (optional)   1 cup chopped pecans or walnuts (optional)   Directions      Preheat the oven to 350 F.   In a medium bowl, whisk flour with baking soda, nutmeg and salt.   In a large bowl, beat butter with sugar and brown sugar until creamy and light. Add vanilla and eggs, one at a time, and mix until incorporated.   Gradually add dry mixture into the butter-sugar wet blend, mixing with a spatula until combined. Add chocolate chips and nuts until just mixed.   Drop tablespoon-sized clumps onto un-greased cookie sheets. Bake for 8-12 minutes, or until pale brown. Allow to cool on the pan for a minute or three, then transfer cookies to a wire rack to finish cooling.",
+        "categories": [],
+        "date": "2025-03-07",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/recipes/chocolate-chip-cookies",
+        "img": null
+      },{
+        "title": "Oatmeal Cookies",
+        "subtitle": "",
+        "excerpt": "Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts.",
+        "content": "Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts.   Ingredients      1 cup butter, softened 1 cup white sugar   1 cup packed brown sugar   2 eggs   1 teaspoon vanilla extract   2 cups all-purpose flour   1 teaspoon baking soda   1 teaspoon salt   1 1/2 teaspoons ground cinnamon   3 cups quick cooking oats   Directions      In a medium bowl, cream together butter, white sugar, and brown sugar. Beat in eggs one at a time, then stir in vanilla. Combine flour, baking soda, salt, and cinnamon; stir into the creamed mixture. Mix in oats. Cover, and chill dough for at least one hour.   Preheat the oven to 375 degrees F (190 degrees C). Grease cookie sheets. Roll the dough into walnut sized balls, and place 2 inches apart on cookie sheets. Flatten each cookie with a large fork dipped in sugar.   Bake for 8 to 10 minutes in preheated oven. Allow cookies to cool on baking sheet for 5 minutes before transferring to a wire rack to cool completely.",
+        "categories": [],
+        "date": "2025-03-07",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/recipes/oatmeal-cookies",
+        "img": null
+      },{
+        "title": "Peanut Butter Cookies",
+        "subtitle": "",
+        "excerpt": "A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s.",
+        "content": "A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s.   Ingredients      1 cup unsalted butter   1 cup crunchy peanut butter   1 cup white sugar   1 cup packed brown sugar   2 eggs 2   1/2 cups all-purpose flour   1 teaspoon baking powder   1/2 teaspoon salt   1 1/2 teaspoons baking soda   Directions      Cream butter, peanut butter, and sugars together in a bowl; beat in eggs.   In a separate bowl, sift flour, baking powder, baking soda, and salt; stir into butter mixture. Put dough in refrigerator for 1 hour.   Roll dough into 1 inch balls and put on baking sheets. Flatten each ball with a fork, making a crisscross pattern. Bake in a preheated 375 degrees F oven for about 10 minutes or until cookies begin to brown.",
+        "categories": [],
+        "date": "2025-03-07",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/recipes/peanut-butter-cookies",
+        "img": null
+      },{
+        "title": "Foo Bar Identity",
+        "subtitle": "",
+        "excerpt": "Foo Bar design system including logo mark, website design, and branding applications.",
+        "content": "Pictures of Goats section West Seattle Blog dingbat newspaper rubber cement Google+ newsroom cafe news.me rubber cement, Ushahidi Kindle Single syndicated Instagram HuffPo community mthomps, Mozilla iPhone app should isn’t a business model curmudgeon Snarkmarket Tim Carmody production of innocence. Fuego tweets community DocumentCloud metered model Gardening &amp; War section YouTube social media SEO information overload analytics Aron Pilhofer Journal Register data visualization WikiLeaks Groupon, collaboration Steve Jobs we need a Nate Silver AP What Would Google Do the power of the press belongs to the person who owns one Clay Shirky curmudgeon Voice of San Diego free as in beer dead trees the notion of the public Lucius Nieman.                       hackgate copyright Lucius Nieman CNN leaves it there right-sizing a giant stack of newspapers that you’ll never read net neutrality algorithms RT algorithms TechCrunch 5% corruption, horse-race coverage Gardening &amp; War section CTR try PR CPC David Cohn shoot a photo algorithms content is king Android Snarkmarket crowdfunding, Fuego Twitter topples dictators YouTube abundance WordPress Reuters try PR stupid commenters should isn’t a business model bringing a tote bag to a knife fight.",
+        "categories": [],
+        "date": "2025-03-07",
+        "tags": [],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/portfolio/foo-bar-website",
+        "img": "https://dieghernan.github.io/chulapa-jekyll-sample/images/portfolio/unsplash-gallery-image-1.jpg"
+      },{
+        "title": "Layout default base image",
+        "subtitle": "Base header with image",
+        "excerpt": "Layout test, header base with image",
+        "content": "Layout test, header base with image",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["base-header","images"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-base-img",
+        "img": "https://picsum.photos/seed/50/750/325"
+      },{
+        "title": "Layout default base",
+        "subtitle": "Base header with no image",
+        "excerpt": "Layout test, header base with no image",
+        "content": "Layout test, header base with no image",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["base-header"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-base",
+        "img": null
+      },{
+        "title": "Layout default hero image",
+        "subtitle": "Hero header with image",
+        "excerpt": "Layout test, header hero with image",
+        "content": "Layout test, header hero with image",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["hero-header","images"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-hero-img",
+        "img": "https://picsum.photos/seed/50/750/325"
+      },{
+        "title": "Layout default hero",
+        "subtitle": "Hero header with image",
+        "excerpt": "Layout test, header hero",
+        "content": "Layout test, header hero",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["hero-header"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-hero",
+        "img": null
+      },{
+        "title": "Layout default image image",
+        "subtitle": "Image header with image",
+        "excerpt": "Layout test, header image with image",
+        "content": "Layout test, header image with image",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["image-header","images"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-image-img",
+        "img": "https://picsum.photos/seed/50/750/325"
+      },{
+        "title": "Layout default image",
+        "subtitle": "Image header",
+        "excerpt": "Layout test, header image",
+        "content": "Layout test, header image",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["image-header"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-image",
+        "img": null
+      },{
+        "title": "Layout default none",
+        "subtitle": "This should appear in archive-like widgets",
+        "excerpt": "I appear always",
+        "content": "I appear always",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["none-header","images"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-none-img",
+        "img": "https://picsum.photos/seed/50/750/325"
+      },{
+        "title": "Layout default post image",
+        "subtitle": "Post header with image",
+        "excerpt": "Layout test, header post with image",
+        "content": "Layout test, header post with image",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["post-header","images"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-post-img",
+        "img": "https://picsum.photos/seed/50/750/325"
+      },{
+        "title": "Layout default post",
+        "subtitle": "Post header with no image",
+        "excerpt": "Layout test, header post with no image",
+        "content": "Layout test, header post with no image",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["post-header"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-post",
+        "img": null
+      },{
+        "title": "Layout default splash image",
+        "subtitle": "Splash header with image",
+        "excerpt": "Layout test, header splash with image",
+        "content": "Layout test, header splash with image",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["splash-header","images"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-splash-img",
+        "img": "https://picsum.photos/seed/50/750/325"
+      },{
+        "title": "Layout default splash",
+        "subtitle": "Splash header",
+        "excerpt": "Layout test, header splash",
+        "content": "Layout test, header splash",
+        "categories": ["layout"],
+        "date": "2025-03-07",
+        "tags": ["splash-header"],
+        "url": "https://dieghernan.github.io/chulapa-jekyll-sample/headers/default-splash",
+        "img": null
       }]

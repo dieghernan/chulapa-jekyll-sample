9ccb6570f30ce3fc2032c9c04a82fccdd666f122
dieghernan <diego.hernangomezherrero@gmail.com>
2025-03-07 05:08:25 +0000

    Deploying to gh-pages from @ dieghernan/chulapa-jekyll-sample@30233e3c0aa7cf37d401ef3c164e8bf06746e130 🚀

--- assets/js/fusejs/fusejs-store.js
+++ assets/js/fusejs/fusejs-store.js
@@ -317,7 +317,7 @@ var store = [{
         "title": "Layout: Excerpt (Defined)",
         "subtitle": "",
         "excerpt": "This is a user-defined post excerpt. It should be displayed in place of the post content in archive-index pages.",
-        "content": "This is the post content. It should be displayed in place of the user-defined excerpt in archive-index pages.   This paragraph should be absent from an archive-index page where post.excerpt is shown.",
+        "content": "This is the post content.   This paragraph should be absent from an archive-index page where post.excerpt is shown.",
         "categories": ["Layout","Uncategorized"],
         "date": "2012-03-14",
         "tags": ["content","excerpt","layout"],
